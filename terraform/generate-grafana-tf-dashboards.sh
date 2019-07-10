8f08fa4e4bf5a7c34326306ea4a5c36ae03e662b
Miles Matthias <mmatthias@google.com>
2019-07-10 14:28:14 -0700

    update terraform dashboard generator script to utilize uid

--- terraform/generate-grafana-tf-dashboards.sh
+++ terraform/generate-grafana-tf-dashboards.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
-echo "Getting the latest Spinnaker Grafana dashboards and adding to Terraform..."
+echo -n "Getting the latest Spinnaker Grafana dashboards and adding to Terraform..."
 
-VERSION=0.13.0
-
-curl -sL0 -o spinnaker-monitoring.zip https://github.com/spinnaker/spinnaker-monitoring/archive/version-$VERSION.zip
+curl -sL0 -o spinnaker-monitoring.zip https://github.com/spinnaker/spinnaker-monitoring/archive/master.zip
 
 unzip -qo spinnaker-monitoring.zip
+
+echo "done."
 
 # apply our dashboard configmap with label 'grafana_dashboard'
 #   so the dashboard sidecar will pick it up and add the dashboard
 
-DASH_DIR=spinnaker-monitoring-version-$VERSION/spinnaker-monitoring-third-party/third_party/prometheus
+echo -n "Generating dashboards in Terraform (HCL) format in 'generated' directory..."
+
+DASH_DIR=spinnaker-monitoring-master/spinnaker-monitoring-third-party/third_party/prometheus
 for filename in $DASH_DIR/*-dashboard.json; do
   fn_only=$(basename $filename)
   fn_root="${fn_only%.*}"
   dest_file="generated/${fn_root}.tf"
+  uid=$(uuidgen)
 
   cat grafana-dashboard.tf.template | sed -e "s/%DASHBOARD%/${fn_root}/" > $dest_file
   printf "    ${fn_only} = \"" >> $dest_file
 
-  cat $filename | sed -e "/\"__inputs\"/,/],/d" \
+  cat $filename | sed -e "s/\"uid\": null/\"uid\": \"${uid}\"/" \
+    | sed -e "/\"__inputs\"/,/],/d" \
       -e "/\"__requires\"/,/],/d" \
       -e "s/\${DS_SPINNAKER}/Prometheus/g" \
       -e "s/\"/\\\\\"/g" \
@@ -33,3 +37,4 @@ for filename in $DASH_DIR/*-dashboard.json; do
   echo -en "  }\n}" >> $dest_file
 done
 
+echo "done."
