a90ff74996233a5801edf4af6d2b489bdffc9d63
Miles Matthias <mmatthias@google.com>
2019-10-27 13:08:51 -0600

    fix prometheus operator connect pod name & add connect for grafana

--- helm/connect.sh
+++ helm/connect.sh
@@ -6,5 +6,8 @@ kubectl port-forward --namespace default $DECK_POD 9000 &
 GATE_POD=$(kubectl get pods --namespace default -l "cluster=spin-gate" -o jsonpath="{.items[0].metadata.name}")
 kubectl port-forward --namespace default $GATE_POD 8084 &
 
-PROM_POD=$(kubectl get pods --namespace default -l "app=prometheus-operator-prometheus" -o jsonpath="{.items[0].metadata.name}")
+PROM_POD=$(kubectl get pods --namespace default -l "app=prometheus" -o jsonpath="{.items[0].metadata.name}")
 kubectl port-forward --namespace default $PROM_POD 9090 &
+
+GRAFANA_POD=$(kubectl get pods --namespace default -l "app=grafana" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward --namespace default $GRAFANA_POD 3000 &
