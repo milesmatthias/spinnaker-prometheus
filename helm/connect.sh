7a98de8b84c13ce084a54858425887c37c86c9b1
Miles Matthias <mmatthias@google.com>
2019-10-27 13:09:42 -0600

    add a note for default grafana creds

--- helm/connect.sh
+++ helm/connect.sh
@@ -11,3 +11,5 @@ kubectl port-forward --namespace default $PROM_POD 9090 &
 
 GRAFANA_POD=$(kubectl get pods --namespace default -l "app=grafana" -o jsonpath="{.items[0].metadata.name}")
 kubectl port-forward --namespace default $GRAFANA_POD 3000 &
+
+echo "Grafana default creds are admin / prom-operator"
